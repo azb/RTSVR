947cf78537fec7227cb3c608ae9c1be78abb87ec
Arthur Baney <azbaney@gmail.com>
2017-02-25 11:28:31 -0800

    Tank can be selected, next up movement

---
+++ RTSVR/Assets/SelectableUnit.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SelectableUnit : MonoBehaviour {
+
+    [SerializeField] private bool selected;
+    [SerializeField] Vector3 moveTo;
+    public const float MovementThreshold = 20.0f;
+    public bool Selected
+    {
+        get
+        {
+            return selected;
+        }
+
+        set
+        {
+            selected = value;
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+        Selected = false;
+    }
+	void Update()
+    {
+        Renderer[] rends = gameObject.GetComponentsInChildren<Renderer>();
+        if (selected)
+        {
+            foreach(Renderer r in rends)
+            {
+                r.material.SetColor("_EmissionColor", new Color(50, 50, 0));
+            }
+        }
+        else
+        {
+            foreach (Renderer r in rends)
+            {
+                r.material.SetColor("_EmissionColor", new Color(0, 0, 0));
+            }
+        }
+        if(selected)
+        {
+            moveTo = new Vector3(Unit_Selection.TargetVector.x, Unit_Selection.TargetVector.y, Unit_Selection.TargetVector.z);
+        }
+        if(moveTo != null)
+        {
+            MoveTo(moveTo);
+        }
+    }
+
+    private void MoveTo(Vector3 target)
+    {
+        Transform[] transf = gameObject.GetComponentsInChildren<Transform>();
+
+        foreach(Transform t in transf)
+        {
+            if (Vector3.Distance(t.position, target) >= MovementThreshold)
+            {
+                t.position = Vector3.Lerp(t.position, target, MovementThreshold / 2);
+            }
+        }
+    }
+}
---
+++ RTSVR/Assets/Unit_Selection.cs
@@ -0,0 +1,103 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Unit_Selection : MonoBehaviour {
+
+    [SerializeField] private SteamVR_TrackedController c;
+
+    private bool wasPressed = false;
+    private LineRenderer lineRenderer;
+    private RaycastHit hit;
+    private RaycastHit hitTarget;
+
+    private int lengthOfRender;
+    private Vector3[] points; //= new Vector3[lengthOfRender];
+    private Vector3 forward;
+
+    public static Vector3 TargetVector;
+    //public static bool readUpdate = false;
+
+    // Use this for initialization
+    void Start () {
+        lengthOfRender = 2;
+        points = new Vector3[lengthOfRender];
+        forward = gameObject.transform.TransformDirection(Vector3.forward) * 10;
+        c = GetComponent<SteamVR_TrackedController>();
+        lineRenderer = GetComponent<LineRenderer>();
+        points[0] = gameObject.transform.position;
+        points[1] = forward;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        SelectUnits();
+        forward = gameObject.transform.TransformDirection(Vector3.forward) * 10;
+        if (c.triggerPressed)
+        {
+            Debug.Log("Here4");
+
+            if (!wasPressed)
+            {
+                Debug.Log("Here3");
+
+                OnSelect();
+            }
+            wasPressed = true;
+        }
+        else
+        {
+            wasPressed = false;
+        }
+        //readUpdate = false;
+        
+    }
+
+    private void SelectUnits()
+    {
+        points[0] = gameObject.transform.position;
+        points[1] = forward;
+        lineRenderer.SetPositions(points);
+        //Check if the controller is pointing at a unit
+        
+    }
+
+
+    private void OnSelect()
+    {
+        
+        if (Physics.CapsuleCast(points[0], points[1], .05f, forward, out hit))
+        {
+            Debug.Log("Here1");
+
+            //Check if trigger is pressed when pointing at unit
+            if (c.triggerPressed /*&& c.controllerIndex == 2*/)
+            {
+                Debug.Log("Here2");
+
+                //Select unit when trigger is pressed while pointing at it
+
+                var su = hit.transform.gameObject.GetComponentInParent<SelectableUnit>();
+                su.Selected = !su.Selected;
+
+                Debug.Log(su.name); 
+                //hit.transform.gameObject.GetComponent<Renderer>().material.SetColor("_EmissionColor", new Color(255, 0, 0)); //MOVED TO SELECTABLEUNIT CLASS
+            }
+        }
+        //Debug.DrawRay(transform.position, forward, Color.green, 2.0f);
+        //lineRenderer.SetPosition(0, forward);
+
+    }
+
+    private void SelectTarget()
+    {
+        if (Physics.Raycast(points[0], points[1], out hitTarget))
+        {
+            //Check if trigger is pressed when pointing at unit
+            if (c.triggerPressed && c.controllerIndex == 1)
+            {
+                //readUpdate = true;
+                TargetVector = hitTarget.point;
+            }
+        }
+    }
+}
